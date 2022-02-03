de8e64c007193b5975fa63e4ca0529dc1a2e14bf
Lucas Aguiar <lucascostaaguiar@hotmail.com>
2022-02-02 22:39:53 -0300

    H2 database, CategoryResource and CategoryServices

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -4,14 +4,19 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.io.Serializable;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "tb_category")
 public class Category implements Serializable {
     private static final  long serialVersionUID =1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private  Long id ;
     private String name ;
 
