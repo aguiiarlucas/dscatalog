57e845f87626e70b035afa4a5989976e73898e66
Lucas Aguiar <lucascostaaguiar@hotmail.com>
2022-03-18 20:45:39 -0300

    Test findById

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -8,11 +8,7 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
-@Getter
-@Setter
-@ToString
-@RequiredArgsConstructor
-@AllArgsConstructor
+
 @Entity
 @Table(name = "tb_category")
 public class Category implements Serializable {
@@ -22,6 +18,32 @@ public class Category implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private  Long id ;
     private String name ;
+
+    public Category(Long id, String name) {
+
+        this.id = id;
+        this.name = name;
+    }
+
+    public Category() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     @Getter
     @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
--- backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
@@ -2,7 +2,9 @@ package com.devsuperior.dscatalog.repositories;
 
 
 import com.devsuperior.dscatalog.entities.Product;
+import com.devsuperior.dscatalog.tests.Factory;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -12,29 +14,52 @@ import java.util.Optional;
 
 @DataJpaTest // Carrega somente os componentes relacionado ao Spring JPA
 public class ProductRepositoryTests {
-
     @Autowired
     private ProductRepository repository;
 
-    @Test
-    public  void deleteShouldDeleteObjectWhenIdExists(){
-        //AAA
-        long existingId = 1L; //Arange
-        repository.deleteById(existingId); //Act
-        Optional<Product>result = repository.findById(existingId);
-        Assertions.assertFalse(result.isPresent());// Assertion
+    private long existingId;
+    private long nonExistingId;
+    private long countTotalProducts;
 
+    @BeforeEach
+    void setUp() throws Exception {
+        existingId = 1L;
+        nonExistingId = 1000L;
+        countTotalProducts = 25L;
     }
     @Test
-    public void deleteShouldThrowEmptyResultDataAccessExceptionWhenIdNotExists(){
-        long  nonExistingId = 1000L;
-        Assertions.assertThrows(EmptyResultDataAccessException.class,()->{
+    public void saveShouldPersistWhitAutoIncrementWhenIdIsNull(){
+        Product product = Factory.createProduct();
+        product.setId(null);
+        product = repository.save(product);
+        Assertions.assertNotNull(product.getId());
+        Assertions.assertEquals(countTotalProducts+1,product.getId());
+
+    }
+
+
+    @Test
+    public void findByIdShouldReturnNonEmptyOptionalWhenIdExists(){
+         Optional<Product>result = repository.findById(existingId);
+         Assertions.assertTrue(result.isEmpty());
+    }
+    @Test
+    public void findByIdShouldReturnEmptyOptionalWhenIdDoesNotExists(){
+
+        Optional<Product> result = repository.findById(nonExistingId);
+        Assertions.assertTrue(result.isEmpty());
+    }
+    @Test
+    public void deleteShouldDeleteObjectWhenIdExists() {
+        repository.deleteById(existingId); //Act
+        Optional<Product> result = repository.findById(existingId);
+        Assertions.assertFalse(result.isPresent());// Assertion
+    }
+
+    @Test
+    public void deleteShouldThrowEmptyResultDataAccessExceptionWhenIdNotExists() {
+        Assertions.assertThrows(EmptyResultDataAccessException.class, () -> {
             repository.deleteById(nonExistingId);
         });
-
-
     }
-
-
-
 }
