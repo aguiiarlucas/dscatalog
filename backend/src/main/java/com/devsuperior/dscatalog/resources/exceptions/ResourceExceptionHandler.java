62a354932414038a5b765a29f93f7ee1d05434ac
Lucas Aguiar <lucascostaaguiar@hotmail.com>
2022-07-08 16:05:32 -0300

    Custom validation response

--- backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -4,6 +4,7 @@ import com.devsuperior.dscatalog.exceptions.ResourceNotFoundException;
 import com.devsuperior.dscatalog.services.exceptions.DatabaseException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -31,7 +32,7 @@ public class ResourceExceptionHandler {
     public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
 
         StandardError err = new StandardError();
-        HttpStatus status  = HttpStatus.BAD_REQUEST;
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         err.setTimestamp(Instant.now());
         err.setStatus(status.value());
         err.setError("Database exception");
@@ -40,20 +41,26 @@ public class ResourceExceptionHandler {
         return ResponseEntity.status(status).body(err);
 
     }
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<StandardError> database(MethodArgumentNotValidException e, HttpServletRequest request) {
 
-        StandardError err = new StandardError();
-        HttpStatus status  = HttpStatus.UNPROCESSABLE_ENTITY;
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
+
+        ValidationError err = new ValidationError();
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
         err.setTimestamp(Instant.now());
         err.setStatus(status.value());
         err.setError("MethodArgumentNotValidException exception");
         err.setMessage(e.getMessage());
         err.setPath(request.getRequestURI());
+
+        for (FieldError f : e.getBindingResult().getFieldErrors()) {
+            err.addError(f.getField(), f.getDefaultMessage());
+
+        }
+
         return ResponseEntity.status(status).body(err);
 
     }
-
 
 
 }
--- backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/StandardError.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/StandardError.java
@@ -16,7 +16,6 @@ public class StandardError implements Serializable {
     private String error;
     private String message;
     private String path;
-
     public StandardError() {
     }
 
