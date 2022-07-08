dbcb181a69000334b225858720e3f5fe6bfb2bc8
Lucas Aguiar <lucascostaaguiar@hotmail.com>
2022-07-08 13:19:35 -0300

    MethodArgumentNotValidException application

--- backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -4,6 +4,7 @@ import com.devsuperior.dscatalog.exceptions.ResourceNotFoundException;
 import com.devsuperior.dscatalog.services.exceptions.DatabaseException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -39,4 +40,20 @@ public class ResourceExceptionHandler {
         return ResponseEntity.status(status).body(err);
 
     }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<StandardError> database(MethodArgumentNotValidException e, HttpServletRequest request) {
+
+        StandardError err = new StandardError();
+        HttpStatus status  = HttpStatus.UNPROCESSABLE_ENTITY;
+        err.setTimestamp(Instant.now());
+        err.setStatus(status.value());
+        err.setError("MethodArgumentNotValidException exception");
+        err.setMessage(e.getMessage());
+        err.setPath(request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+
+    }
+
+
+
 }
